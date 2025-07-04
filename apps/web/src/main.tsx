b91d47f097f820ddf8229f37a3dca8197f35be99
Nick Fuller <nick@pfsbc.com>
2025-07-04 16:09:43 -0700

    create the react app

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,23 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+const rootEle = document.getElementById("root");
+if (rootEle && !rootEle.innerHTML) {
+	const root = ReactDOM.createRoot(rootEle);
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
+}
---
+++ apps/web/src/mocks/handlers.ts
@@ -0,0 +1,13 @@
+import { HttpResponse, http } from "msw";
+
+// Example/Placeholder handler
+// https://mswjs.io/docs/best-practices/structuring-handlers
+export const handlers = [
+	http.get("*/api/v1/users", () => {
+		return HttpResponse.json({
+			id: "this-is-the-way",
+			firstName: "The",
+			lastName: "Mandalorian",
+		});
+	}),
+];
