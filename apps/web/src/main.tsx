2762dcac76f0f62ac336a21ddf7eb217d4979cb3
Nick Fuller <nick@pfsbc.com>
2025-07-04 16:32:22 -0700

    remove unused import

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./report-web-vitals";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
